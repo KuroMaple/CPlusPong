d8df16ecafe41e1711db2a296e1798431e65870d
Hassan Hashmi <hashmihassan47@gmail.com>
2025-11-02 13:19:27 -0500

    Initial Commit

---
+++ CPlusPong.cpp
@@ -0,0 +1,12 @@
+﻿// CPlusPong.cpp : Defines the entry point for the application.
+//
+
+#include "CPlusPong.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
